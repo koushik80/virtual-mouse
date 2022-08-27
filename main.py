beb30a347a0b6e00b22713d8352300cacd7ab786
koushik <koushik.dey@live.com>
2022-08-27 23:59:18 +0300

    eyebrow distane and movement detection

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import pyautogui
 
 cam = cv2.VideoCapture(0)
 face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
+screen_w, screen_h = pyautogui.size()
 while True:
     _, frame = cam.read()
     frame = cv2.flip(frame, 1)
@@ -22,7 +23,15 @@ while True:
             # print(x, y)
             cv2.circle(frame, (x, y), 3, (0, 255, 0))
             if id == 1:
-                pyautogui.moveTo(x, y)
+                screen_x = screen_w * landmark.x
+                screen_y = screen_h * landmark.y
+                pyautogui.moveTo(screen_x, screen_y)
+        left = [landmarks[145], landmarks[159]]
+        for landmark in left:
+            x = int(landmark.x * frame_w)
+            y = int(landmark.y * frame_h)
+            cv2.circle(frame, (x, y), 3, (0, 255, 255))
+        print(left[0].y - left[1].y)
 
     cv2.imshow('Eye-controlled-mouse', frame)
     cv2.waitKey(1)
