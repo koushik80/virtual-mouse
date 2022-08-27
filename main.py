6ca8825beb9479733404dcae81d7905fef7ca2dd
koushik <koushik.dey@live.com>
2022-08-28 00:10:29 +0300

    fnl

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pyautogui
 cam = cv2.VideoCapture(0)
 face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
 screen_w, screen_h = pyautogui.size()
+
 while True:
     _, frame = cam.read()
     frame = cv2.flip(frame, 1)
@@ -14,6 +15,7 @@ while True:
     landmark_points = output.multi_face_landmarks
     frame_h, frame_w, _ = frame.shape
     # print(landmark_points)
+
     if landmark_points:
         landmarks = landmark_points[0].landmark
         for id, landmark in enumerate(landmarks[474:478]):
@@ -27,11 +29,17 @@ while True:
                 screen_y = screen_h * landmark.y
                 pyautogui.moveTo(screen_x, screen_y)
         left = [landmarks[145], landmarks[159]]
+
         for landmark in left:
             x = int(landmark.x * frame_w)
             y = int(landmark.y * frame_h)
             cv2.circle(frame, (x, y), 3, (0, 255, 255))
-        print(left[0].y - left[1].y)
+        # print(left[0].y - left[1].y)
+
+        if (left[0].y - left[1].y) < 0.004:
+            # print('click')
+            pyautogui.click()
+            pyautogui.sleep(1)
 
     cv2.imshow('Eye-controlled-mouse', frame)
     cv2.waitKey(1)
