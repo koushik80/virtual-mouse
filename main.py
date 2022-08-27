16bdec26c0a30b352f91649a4175d7d562c3419d
koushik <koushik.dey@live.com>
2022-08-27 22:52:10 +0300

    face_detection

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from asyncore import read
 import cv2
+import mediapipe as mp
 
 
 cam = cv2.VideoCapture(0)
+face_mesh = mp.solutions.face_mesh.FaceMesh()
 while True:
     _, frame = cam.read()
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    output = face_mesh.process(rgb_frame)
+    landmark_points = output.multi_face_landmarks
+
     cv2.imshow('Eye-controlled-mouse', frame)
     cv2.waitKey(1)
